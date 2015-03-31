2c34dba7e1cf394ada5503c7bdb7cef8082d9be3
KonstantinAdamu <KonstantinAdamu@users.noreply.github.com>
2015-03-31 20:09:29 +0300

    Custom exception class added
    UnhealthyException added

---
+++ HealthBuddy/HealthBuddy/Exceptions/UnhealthyException.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HealthBuddy.Exceptions
+{
+    using System;
+    using System.Windows;
+    public class UnhealthyException : Exception
+    {
+        //TODO: Develop exception message...
+        public UnhealthyException() : base() { }
+        public UnhealthyException(string message)
+            : base(message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
